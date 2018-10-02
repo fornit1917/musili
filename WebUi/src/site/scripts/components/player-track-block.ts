caa3ae814d2b49f2ff2a8aca44b358f0b5ea0a7b
Vit S <fornit1917@gmail.com>
2018-10-02 15:45:50 +0300

    show track info

--- WebUi/src/site/scripts/components/player-track-block.ts
+++ WebUi/src/site/scripts/components/player-track-block.ts
@@ -3,10 +3,14 @@ import { Track } from "../dto-types";
 export default class PlayerTrackBlock {
     private spinner: HTMLElement;
     private trackInfo: HTMLElement;
+    private artist: HTMLElement;
+    private title: HTMLElement;
 
     public constructor (root: HTMLElement) {
         this.spinner = root.querySelector(".js-spinner");
         this.trackInfo = root.querySelector(".js-track-info");
+        this.artist = this.trackInfo.querySelector(".js-artist");
+        this.title = this.trackInfo.querySelector(".js-title");
     }
 
     public showLoadingIndicator() {
@@ -17,5 +21,7 @@ export default class PlayerTrackBlock {
     public showTrackInfo(track: Track) {
         this.trackInfo.style.display = "block";
         this.spinner.style.display = "none";
+        this.artist.innerHTML = track.artist;
+        this.title.innerHTML = track.title;
     }
 }
