f136f88a8b78e14205783c22eabcd523071b53cd
Vit S <fornit1917@gmail.com>
2019-05-14 21:19:48 +0300

    add NLog

--- WebApi/Controllers/TestController.cs
+++ WebApi/Controllers/TestController.cs
@@ -8,16 +8,19 @@ using Musili.WebApi.Interfaces;
 using Musili.WebApi.Models;
 using Musili.WebApi.Utils;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace Musili.WebApi.Controllers {
     [Route("api/[controller]")]
     public class TestController : Controller {
         private ITracksProvider _tracksProvider;
         private AppDbContext _db;
+        private readonly ILogger<TestController> _logger;
 
-        public TestController(AppDbContext db, ITracksProvider tracksProvider) {
+        public TestController(AppDbContext db, ITracksProvider tracksProvider, ILogger<TestController> logger) {
             _db = db;
             _tracksProvider = tracksProvider;
+            _logger = logger;
         }
 
         [HttpGet("source")]
@@ -28,6 +31,13 @@ namespace Musili.WebApi.Controllers {
         [HttpGet("")]
         public object Index() {
             return new Object();
+        }
+
+        [HttpGet("logs")]
+        public string Logs() {
+            _logger.LogInformation("Test info message");
+            _logger.LogError("Test error message");
+            return "Ok";
         }
     }
 }
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -7,16 +7,31 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
 
 namespace Musili.WebApi {
     public class Program {
         public static void Main(string[] args) {
-            BuildWebHost(args).Run();
+            Logger logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try {
+                logger.Info("Init application...");
+                BuildWebHost(args).Run();
+            } catch (Exception ex) {
+                logger.Error(ex, "Stopped program because of exception");
+            } finally {
+                LogManager.Shutdown();
+            }
+            
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .ConfigureLogging(logging => {
+                    logging.ClearProviders();
+                })
+                .UseNLog()
                 .Build();
     }
 }
