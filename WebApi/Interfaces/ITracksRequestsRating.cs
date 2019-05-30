b5f7facb8245d387bebfec82d525cbf7df53e4ce
Vit S <fornit1917@gmail.com>
2019-05-30 22:33:14 +0300

    Load tracks in background for hot criterias, begin. Add tracks criterias rating service

---
+++ WebApi/Interfaces/ITracksRequestsRating.cs
@@ -0,0 +1,10 @@
+﻿using Musili.WebApi.Models;
+using System;
+
+namespace Musili.WebApi.Interfaces {
+    public interface ITracksRequestsRating {
+        void AddCriteria(TracksCriteria criteria, DateTime? dateTime = null);
+        TracksCriteria[] GetHotCriterias(DateTime minDate);
+        int RemoveOldRequests(DateTime minDate);
+    }
+}
